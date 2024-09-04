0a5af67063cdca64c8735d657e73c8304c6998dd
Renan Costa <renancostam@gmail.com>
2024-09-04 15:18:20 -0300

    Adding some paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <Header />
       <main className="pt-24 lg:pt-48 flex min-h-screen flex-col">
-        <div className="max-w-4xl container flex flex-col gap-10 lg:gap-16">
+        <div className="max-w-[922px] container flex flex-col gap-10 lg:gap-16 pb-16 lg:pb-24">
           <Pronouncement />
           <Proposal />
         </div>
