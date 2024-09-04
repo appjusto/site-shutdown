5413df7d9b793bb4bc38a5eaed02fd68d2f908b6
Renan Costa <renancostam@gmail.com>
2024-09-04 15:25:57 -0300

    Adding firebase deploy configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
